64d010623e857295d53a4af5d547dfb749074371
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:34:25 -0300

    Mensagem para cadastro com mesmo cpf

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -10,18 +10,22 @@ import { AlunoService } from './aluno.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- aluno: Aluno = {nome: "", cpf: "", email: "", login: ""};
- alunoService = new AlunoService();
- alunos: Aluno[] = [];
+   aluno: Aluno = {nome: "", cpf: "", email: "",login: ""};
+   alunoService = new AlunoService();
+   alunos: Aluno[] = [];
+   cpfduplicado: boolean = false;
+
 
    gravar(a: Aluno): void {
      if (this.alunoService.gravar(a)) {
        this.alunos.push(a);
        this.aluno = {nome: "", cpf: "", email: "", login: ""};
      } else {
-       this.aluno.cpf = "";
-       alert("Já existe um aluno com esse CPF");
+       this.cpfduplicado = true;
      }
+  }
+  onMove(): void {
+      this.cpfduplicado = false;
   }
 }
 
