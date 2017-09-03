250f6e3268bb15e31f72aa7f1147c34426aa791c
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:13:56 -0300

    Adição do botão 'Adicionar'

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 
+import { Aluno } from './aluno';
+import { AlunoService } from './aluno.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +11,11 @@ import { NgModule } from '@angular/core';
 })
 export class AppComponent {
  aluno: Aluno = {nome: "", cpf: "", email: "", login: ""};
+ alunoService = new AlunoService();
+  
+   gravar(a: Aluno): void {
+     this.alunoService.gravar(a);
+     this.aluno = {nome: "", cpf: "", email: "", login: ""};
+  }
 }
 
-export class Aluno {
-  nome: string = "";
-  cpf: string = "";
-  email: string = "";
-  login: string = "";
-}
